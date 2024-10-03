fd6458aa8d49dd6ccefd08fcde636aa8b3e8ddc9
BarbieMunoz <a01659348@tec.mx>
2024-10-03 14:35:58 -0600

    changed image

--- Detect_Anotate.py
+++ Detect_Anotate.py
@@ -4,7 +4,8 @@ import supervision as sv
 
 #obtain predictions from YOLO
 model = YOLO("yolov8n.pt")
-image = cv2.imread("image1_road.jpeg")
+image = cv2.imread("image2.png")
+image_height, image_width, _ = image.shape
 results = model(image)[0]
 
 # load them into Supervision.
@@ -12,7 +13,7 @@ detections = sv.Detections.from_ultralytics(results)
 
 # annotate the image
 box_annotator = sv.BoxAnnotator()
-label_annotator = sv.LabelAnnotator()
+label_annotator = sv.LabelAnnotator(text_scale=0.3,  text_thickness=1)
 
 labels = [
     f"{class_name} {confidence:.2f}"
@@ -20,7 +21,17 @@ labels = [
     in zip(detections['class_name'], detections.confidence)
 ]
 
-annotated_image = box_annotator.annotate(
-    scene=image, detections=detections)
-annotated_image = label_annotator.annotate(
-    scene=annotated_image, detections=detections, labels=labels)
+annotated_image = box_annotator.annotate(scene=image, detections=detections)
+annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+
+# resize immage
+annotated_image = cv2.resize(annotated_image, (image_width, image_height), interpolation=cv2.INTER_LINEAR)
+
+# resize window
+cv2.namedWindow("Annotated Image", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Annotated Image", 1200, 800)
+
+# displa in a window
+cv2.imshow("Annotated Image", annotated_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
